6ef1240e9536deec9573b170cf608fddc4571c02
Whitedot-6859 <160409654+Whitedot-6859@users.noreply.github.com>
2024-02-22 21:15:21 +0600

    Update 00sexy.js

--- modules/commands/00sexy.js
+++ modules/commands/00sexy.js
@@ -15,7 +15,7 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
