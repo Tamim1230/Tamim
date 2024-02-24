87ddfa62e7799b41fa079bd84085961a3bc8f91b
Tamim110453 <161113290+Tamim110453@users.noreply.github.com>
2024-02-24 15:11:00 +0600

    Update 00lf.js

--- modules/commands/00lf.js
+++ modules/commands/00lf.js
@@ -20,7 +20,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["Lofi Status // 「 𝐑𝟎𝐌!𝐌 」 🌺"];
+   var hi = ["Lofi Status // 「 T4M1M 」 🌺"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
